d37e178b560e5f987e601021b9aa489744525df6
Mimi Cong <mina123fire@yahoo.com>
2020-08-25 22:16:09 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -17,13 +17,13 @@ function nowServing(katzDeliLine) {
 
 function currentLine(katzDeliLine) {
   if (katzDeliLine.length>0) {
-    return "The line is currently empty."
-  } else {
     var currentLineStr = "The line is currently: ";
     for (let i=0;i<katzDeliLine.length;i++) {
       currentLineStr = currentLineStr + "${i}. ${katzDeliLine[i]}, ";
     }
     return currentLineStr;
+  } else {
+    return "The line is currently empty."
   }
   
 }
