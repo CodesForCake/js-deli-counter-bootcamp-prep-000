e79885f08c555ac9855eb0075cb04e7fb9b6816d
Mimi Cong <mina123fire@yahoo.com>
2020-08-25 22:05:16 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,13 +1,15 @@
 var katzDeliLine = [];
 
 function takeANumber(katzDeliLine, newPerson) {
-  return "Welcome, "+newPerson+". You are number "+katzDeliLine[newPerson]+" in line."
+  return "Welcome, "+newPerson+". You are number "+(katzDeliLine.length+1)+" in line."
 }
 
 function nowServing(katzDeliLine) {
   if (katzDeliLine.length>0){
+    var firstPerson = katzDeliLine.slice(0,1);
     
+    return firstPerson;
   } else {
-    return "There "
+    return "There is nobody waiting to be served!"
   }
 }
