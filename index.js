5568b7517b7d43ea503a8ea3c89160e7e231ee3b
Mimi Cong <mina123fire@yahoo.com>
2020-08-25 22:32:14 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ var katzDeliLine = [];
 
 function takeANumber(katzDeliLine, newPerson) {
   katzDeliLine.push(newPerson);
-  return "Welcome, "+newPerson+". You are number "+katzDeliLine.length+" in line."
+  return `Welcome, ${newPerson}. You are number ${katzDeliLine.length} in line.`
 }
 
 function nowServing(katzDeliLine) {
