2b905c5a9b90aa44cbcea6ed72b9a0cd9b02c361
Mimi Cong <mina123fire@yahoo.com>
2020-08-25 22:18:08 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -19,7 +19,7 @@ function currentLine(katzDeliLine) {
   if (katzDeliLine.length>0) {
     var currentLineStr = "The line is currently: ";
     for (let i=0;i<katzDeliLine.length;i++) {
-      currentLineStr = currentLineStr + "${i}. ${katzDeliLine[i]}, ";
+      currentLineStr = currentLineStr + i +". " + katzDeliLine[i]+", ";
     }
     return currentLineStr;
   } else {
